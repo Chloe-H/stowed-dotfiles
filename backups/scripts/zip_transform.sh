2faa52d925eb5982e2fe8057470574145d5361b2
Chloe-H <chloe.henderson@goilluminate.com>
2019-05-24 14:19:40 -0500

    Added a prompt for a suffix for the archive name
    Also wrapped all variable names in substitution expressions in curly
    braces.

--- backups/scripts/zip_transform.sh
+++ backups/scripts/zip_transform.sh
@@ -7,29 +7,40 @@
 echo -e "\nEnter the site name abbreviation as it appears in the repo folder name (e.g. 'tmc' for 'cls.impl.tmc')"
 read -p "Site name: " siteName
 
-if [ -d "cls.impl.$siteName" ]; then
+# Prompt for optional suffix for output archive's name (I like to use the commit hash)
+read -p "(Optional) Enter a suffix for the archive's name: " archiveSuffix
+
+if [ -d "cls.impl.${siteName}" ]; then
     # Pattern to find the release binaries
     searchPattern="*Softek.Cls.Impl.*[^Test]/bin/Release/Softek.Cls.Impl.*"
 
     # Get the path to the release directory
-    releaseDirectory=$(dirname $(find cls.impl.$siteName -wholename $searchPattern.dll))
+    releaseDirectory=$(dirname $(find cls.impl.${siteName} -wholename ${searchPattern}.dll))
 
     # Get the names of the DLL and PDB files
     declare -a files
-    for file in $(find cls.impl.$siteName -wholename $searchPattern)
-        do files+=($(basename $file))
+    for file in $(find cls.impl.${siteName} -wholename ${searchPattern})
+        do files+=($(basename ${file}))
     done
 
-    # Zip up the DLL and PDB files
+    # By getting only the base names of the files and changing to the release
+    # binaries directory, I am able to avoid duplicating the structure of the
+    # transform's repository in the archive I create.
+
     currentDirectory=$(pwd)
     cd $releaseDirectory
     echo -e "\nZipping up files..."
 
-    # By getting only the base names of the files and changing to the release
-    # binaries directory, we are able to avoid duplicating the structure of the
-    # transform's repository in the archive we create.
-    tar -cvf $currentDirectory/$siteName-transform.tar ${files[@]}
+    archiveName="${currentDirectory}/${siteName}-transform"
+
+    # Add suffix to archive name, if provided
+    if [ -z "${archiveSuffix}" ]; then
+        archiveName="${archiveName}-${archiveSuffix}"
+    fi
+
+    # Zip up the DLL and PDB files
+    tar -cvf ${archiveName}.tar ${files[@]}
 else
-    echo "The directory 'cls.impl.$siteName' does not exist!"
+    echo "The directory 'cls.impl.${siteName}' does not exist!"
 fi
 
