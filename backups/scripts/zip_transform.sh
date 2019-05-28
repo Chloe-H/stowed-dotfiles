23290e732b738bd74934fb25a3658cff75743d8c
Chloe-H <chloe.henderson@goilluminate.com>
2019-05-28 10:00:24 -0500

    Minor improvements and refactoring
    - Leaving the output archive suffix blank will use the short commit hash
    - Added a variable for the transform repository folder name

--- backups/scripts/zip_transform.sh
+++ backups/scripts/zip_transform.sh
@@ -3,44 +3,42 @@
 # This script will zip up the release version of the DLL and PDB file for the
 # specified transform.
 
-# Prompt for suffix of transform repository's directory name
-echo -e "\nEnter the site name abbreviation as it appears in the repo folder name (e.g. 'tmc' for 'cls.impl.tmc')"
+echo -e "\nEnter the site name abbreviation as it appears in the repo folder name (e.g. 'tmc' for 'cls.impl.tmc')."
 read -p "Site name: " siteName
+repoFolder="cls.impl.${siteName}"
 
-# Prompt for optional suffix for output archive's name (I like to use the commit hash)
-read -p "(Optional) Enter a suffix for the archive's name: " archiveSuffix
+echo -e "Enter a suffix for the archive's name or leave it blank to use the short commit hash." 
+read -p "Output archive suffix: " archiveSuffix
 
-if [ -d "cls.impl.${siteName}" ]; then
+if [ -d ${repoFolder} ]; then
     # Pattern to find the release binaries
     searchPattern="*Softek.Cls.Impl.*[^Test]/bin/Release/Softek.Cls.Impl.*"
 
     # Get the path to the release directory
-    releaseDirectory=$(dirname $(find cls.impl.${siteName} -wholename ${searchPattern}.dll))
+    releaseDirectory=$(dirname $(find ${repoFolder} -wholename ${searchPattern}.dll))
 
     # Get the names of the DLL and PDB files
     declare -a files
-    for file in $(find cls.impl.${siteName} -wholename ${searchPattern})
+    for file in $(find ${repoFolder} -wholename ${searchPattern})
         do files+=($(basename ${file}))
     done
 
     # By getting only the base names of the files and changing to the release
     # binaries directory, I am able to avoid duplicating the structure of the
     # transform's repository in the archive I create.
+    outputDirectory=$(pwd)
+    cd ${releaseDirectory}
 
-    currentDirectory=$(pwd)
-    cd $releaseDirectory
-    echo -e "\nZipping up files..."
-
-    archiveName="${currentDirectory}/${siteName}-transform"
-
-    # Add suffix to archive name, if provided
-    if [ -n "${archiveSuffix}" ]; then
-        archiveName="${archiveName}-${archiveSuffix}"
+    # Get the short commit hash (must be in the Git repository)
+    if [ -z "${archiveSuffix}" ]; then
+        archiveSuffix=$(git rev-parse --short HEAD)
     fi
 
-    # Zip up the DLL and PDB files
-    tar -cvf ${archiveName}.tar ${files[@]}
+    archiveName="${outputDirectory}/${siteName}-transform-${archiveSuffix}.tar"
+
+    echo -e "\nZipping up files..."
+    tar -cvf ${archiveName} ${files[@]}
 else
-    echo "The directory 'cls.impl.${siteName}' does not exist!"
+    echo "The directory '${repoFolder}' does not exist!"
 fi
 
